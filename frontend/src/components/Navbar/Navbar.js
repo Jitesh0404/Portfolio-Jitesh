2702dee4968a00c06557c3e09d429114fcab57bc
Jitesh0404 <jitesh04sharma@gmail.com>
2024-03-17 14:57:45 +0530

    Completed the responsiveness of nav

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { TfiMenu } from "react-icons/tfi";
+import { useMediaQuery } from "react-responsive";
 import "./Navbar.css";
 function Navbar() {
   const [navSelected, setNavSelected] = useState("Home");
   console.log("navSelected ", navSelected);
+  const [isMenuActive, setIsMenuActive] = useState(false);
+  console.log("isMenuActive ", isMenuActive);
+  const isMediumDevice = useMediaQuery({ query: "(min-width: 768px)" });
+  useEffect(() => {
+    if (isMediumDevice) {
+      // reset the nav to top when user explicity changes the width of devices
+      setIsMenuActive(false);
+    }
+  }, [isMediumDevice]);
   const handleNavSelection = (option) => {
     switch (option) {
       case "Home":
@@ -28,97 +39,113 @@ function Navbar() {
   const handleMouseOver = () => {};
   return (
     <nav className="w-full justify-between pt-4 fixed bg-black z-10 py-4">
-      <div className="flex items-center justify-between w-3/4 m-auto">
-      <a href="#home" className="text-white text-2xl font-extrabold">
-        Jitesh
-      </a>
-      <div className="flex basis-3/4">
-        <ul className="flex w-full justify-evenly">
-          <li
-            className={`font-medium text-lg ${
-              navSelected === "Home"
-                ? "text-yellow-700 font-normal text-xl"
-                : "text-white"
+      <div className="flex items-center px-20 lg:p-0 justify-between sm:w-full md:w-full lg:w-[70%] m-auto">
+        <a href="#home" className="text-white text-2xl font-extrabold">
+          Jitesh
+        </a>
+        <div
+          className={`lg:block  basis-3/4 ${isMenuActive ? "block" : "hidden"}`}
+        >
+          <ul
+            className={`bg-black flex w-full justify-evenly ${
+              isMenuActive
+                ? "px-2 flex-col absolute left-20 top-20 max-w-fit gap-4 transition-menu mt-10"
+                : ""
             }`}
-            onClick={() => handleNavSelection("Home")}
-            onMouseOver={handleMouseOver}
           >
-            <a href="#home">Home</a>
-          </li>
-          <li
-            className={`font-medium text-lg ${
-              navSelected === "About"
-                ? "text-yellow-700 font-normal text-xl"
-                : "text-white"
-            }`}
-            onClick={() => handleNavSelection("About")}
-          >
-            <a href="#about">About</a>
-          </li>
-          <li
-            className={`font-medium text-lg ${
-              navSelected === "Resume"
-                ? "text-yellow-700 font-normal text-xl"
-                : "text-white"
-            }`}
-            onClick={() => handleNavSelection("Resume")}
-          >
-            <a href="#resume">Resume</a>
-          </li>
-          <li
-            className={`font-medium text-lg ${
-              navSelected === "Services"
-                ? "text-yellow-700 font-normal text-xl"
-                : "text-white"
-            }`}
-            onClick={() => handleNavSelection("Services")}
-          >
-            <a href="#services">Services</a>
-          </li>
-          <li
-            className={`font-medium text-lg ${
-              navSelected === "Skills"
-                ? "text-yellow-700 font-normal text-xl"
-                : "text-white"
-            }`}
-            onClick={() => handleNavSelection("Skills")}
-          >
-            <a href="#skills">Skills</a>
-          </li>
-          <li
-            className={`font-medium text-lg ${
-              navSelected === "Projects"
-                ? "text-yellow-700 font-normal text-xl"
-                : "text-white"
-            }`}
-            onClick={() => handleNavSelection("Projects")}
-          >
-            <a href="#projects">Projects</a>
-          </li>
-          <li
-            className={`font-medium text-lg ${
-              navSelected === "My Blog"
-                ? "text-yellow-700 font-normal text-xl"
-                : "text-white"
-            }`}
-            onClick={() => handleNavSelection("My Blog")}
-          >
-            <a href="#blogs">My Blog</a>
-          </li>
-          <li
-            className={`font-medium text-lg ${
-              navSelected === "Contact"
-                ? "text-yellow-700 font-normal text-xl"
-                : "text-white"
-            }`}
-            onClick={() => handleNavSelection("Contact")}
-          >
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
+            <li
+              className={`font-medium text-lg ${
+                navSelected === "Home"
+                  ? "text-yellow-700 font-normal text-xl underline underline-offset-[10px]"
+                  : "text-white"
+              }`}
+              onClick={() => handleNavSelection("Home")}
+              onMouseOver={handleMouseOver}
+            >
+              <a href="#home">Home</a>
+            </li>
+            <li
+              className={`font-medium text-lg ${
+                navSelected === "About"
+                  ? "text-yellow-700 font-normal text-xl underline-offset-[10px]"
+                  : "text-white"
+              }`}
+              onClick={() => handleNavSelection("About")}
+            >
+              <a href="#about">About</a>
+            </li>
+            <li
+              className={`font-medium text-lg ${
+                navSelected === "Resume"
+                  ? "text-yellow-700 font-normal text-xl underline-offset-[10px]"
+                  : "text-white"
+              }`}
+              onClick={() => handleNavSelection("Resume")}
+            >
+              <a href="#resume">Resume</a>
+            </li>
+            <li
+              className={`font-medium text-lg ${
+                navSelected === "Services"
+                  ? "text-yellow-700 font-normal text-xl underline-offset-[10px]"
+                  : "text-white"
+              }`}
+              onClick={() => handleNavSelection("Services")}
+            >
+              <a href="#services">Services</a>
+            </li>
+            <li
+              className={`font-medium text-lg ${
+                navSelected === "Skills"
+                  ? "text-yellow-700 font-normal text-xl underline-offset-[10px]"
+                  : "text-white"
+              }`}
+              onClick={() => handleNavSelection("Skills")}
+            >
+              <a href="#skills">Skills</a>
+            </li>
+            <li
+              className={`font-medium text-lg ${
+                navSelected === "Projects"
+                  ? "text-yellow-700 font-normal text-xl underline-offset-[10px]"
+                  : "text-white"
+              }`}
+              onClick={() => handleNavSelection("Projects")}
+            >
+              <a href="#projects">Projects</a>
+            </li>
+            <li
+              className={`font-medium text-lg ${
+                navSelected === "My Blog"
+                  ? "text-yellow-700 font-normal text-xl underline-offset-[10px]"
+                  : "text-white"
+              }`}
+              onClick={() => handleNavSelection("My Blog")}
+            >
+              <a href="#blogs">My Blog</a>
+            </li>
+            <li
+              className={`font-medium text-lg ${
+                navSelected === "Contact"
+                  ? "text-yellow-700 font-normal text-xl underline-offset-[10px]"
+                  : "text-white"
+              }`}
+              onClick={() => handleNavSelection("Contact")}
+            >
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <button
+          className="lg:hidden"
+          onClick={() => setIsMenuActive(!isMenuActive)}
+        >
+          <div className="flex flex-row justify-center items-center gap-2 font-bold">
+            <TfiMenu color="white" size={20} className="cursor-pointer" />
+            <h4 className="text-gray-500 text-lg uppercase">Menu</h4>
+          </div>
+        </button>
       </div>
-      </div>
-        
     </nav>
   );
 }
--- frontend/src/components/Screens/ResumePage.jsx
+++ frontend/src/components/Screens/ResumePage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ShadowContainer from "../CommonComponets/ShadowContainer";
 import ResumeComponent from "../CommonComponets/ResumeComponent";
 import ResumeContent from "../constants/resumeText.json";
+import { motion } from "framer-motion";
+import Resume from "../../assets/Jitesh-Sharma-Resume.pdf";
 export default function ResumePage() {
   return (
     <div id="resume" className="h-screen pt-20">
@@ -13,13 +15,36 @@ export default function ResumePage() {
             text="A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth."
           />
         </div>
-        <div className="flex flex-wrap justify-evenly mt-10 gap-6">
-          {
-            ResumeContent.map((data)=>(
-              <ResumeComponent duration={data.duration} position={data.position} companyName={data.companyName} description={data.description}/>
-            ))
-          }
-        </div>
+        <motion.div
+          initial={{
+            opacity: 0,
+            y: 30,
+          }}
+          whileInView={{
+            opacity: 1,
+            y: 0,
+            transition: { duration: 1 },
+          }}
+          viewport={{ once: false, amount: 0.8 }}
+        >
+          <div className="flex flex-wrap justify-evenly mt-10 gap-6">
+            {ResumeContent.map((data) => (
+              <ResumeComponent
+                duration={data.duration}
+                position={data.position}
+                companyName={data.companyName}
+                description={data.description}
+              />
+            ))}
+          </div>
+          <div className="flex justify-center items-center mt-10">
+            <button className="bg-[#FFBD39] px-7 py-5 rounded-3xl font-semibold tracking-[0.2rem] uppercase text-[#000000] text-sm mt-4 hover:opacity-85 items-center">
+              <a href={Resume} download>
+                Download CV
+              </a>
+            </button>
+          </div>
+        </motion.div>
       </div>
     </div>
   );
