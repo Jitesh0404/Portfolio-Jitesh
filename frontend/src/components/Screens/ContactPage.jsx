ecbb22a76f73ba9846a2303b945a13ff29aa5807
Jitesh0404 <jitesh04sharma@gmail.com>
2024-03-27 21:52:12 +0530

    Contact Page completed

--- frontend/src/components/Screens/ContactPage.jsx
+++ frontend/src/components/Screens/ContactPage.jsx
@@ -30,16 +30,16 @@ export default function ContactPage() {
           <Contact Icon={<IoMdCall color="#FFBD39" size={30}/>} heading={'CONTACT NUMBER'} txt={'+91 9929878107'}/>
           <Contact Icon={<IoMdMailUnread color="#FFBD39" size={30}/>} heading={'EMAIL'} txt={'jitesh04sharma@gmail.com'}/>
         </div>
-        <div className=" w-[60%] p-14 m-auto mt-20 bg-[#1A2228] rounded-lg">
+        <div className=" w-[90%] md:w-[80%] lg:w-[60%] p-14 m-auto mt-20 bg-[#1A2228] rounded-lg">
           <h2 className="text-white text-4xl font-bold">Get in touch.</h2>
           <form action="#" className="mt-10 flex flex-col md:flex-row flex-wrap gap-4">
-            <input required type="text" placeholder="First Name " className="p-4 rounded-xl min-w-[40%] text-xl text-gray-700 border-none outline-none"/>
-            <input required type="text" placeholder="Last Name " className="p-4 rounded-xl min-w-[40%] text-xl text-gray-700 border-none outline-none"/>
-            <input required type="email" placeholder="Email " className="p-4 rounded-xl min-w-[40%] text-xl text-gray-700 border-none outline-none"/>
-            <input required type="text" placeholder="Subject" className="p-4 rounded-xl min-w-[40%] text-xl text-gray-700 border-none outline-none"/>
-            <div className="w-[82%] flex gap-4">
-              <textarea required placeholder="Message" className="p-4 rounded-xl text-xl text-gray-700 border-none outline-none basis-2/3"/>
-              <input type="submit" value='Submit' className="p-4 rounded-xl bg-[#FFBD39] text-xl text-gray-700 border-none cursor-pointer basis-1/3"/>
+            <input required type="text" placeholder="First Name " className="w-[90%] md:w-[80%] p-4 rounded-xl text-xl text-gray-700 border-none outline-none"/>
+            <input required type="text" placeholder="Last Name " className="w-[90%] md:w-[80%] p-4 rounded-xl text-xl text-gray-700 border-none outline-none"/>
+            <input required type="email" placeholder="Email " className="w-[90%] md:w-[80%] p-4 rounded-xl text-xl text-gray-700 border-none outline-none"/>
+            <input required type="text" placeholder="Subject" className="w-[90%] md:w-[80%] p-4 rounded-xl text-xl text-gray-700 border-none outline-none"/>
+            <div className="flex flex-col gap-y-4 w-[90%] md:w-[80%]">
+              <textarea required placeholder="Message" className="w-[90%] md:w-[80%] p-4 rounded-xl min-w-[100%] text-xl text-gray-700 border-none outline-none border border-red-700"/>
+              <input type="submit" value='Submit' className="w-[90%] md:w-[80%] p-4 rounded-xl self-center bg-[#FFBD39] text-xl text-white border-none cursor-pointer "/>
             </div>
           </form>
         </div>
